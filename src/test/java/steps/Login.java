7955344e622ad955a032ad03bef02ae6f1b1c30d
taner77 <tcelikim@gmail.com>
2020-11-08 22:25:00 -0500

    step Login update

--- src/test/java/steps/Login.java
+++ src/test/java/steps/Login.java
@@ -10,10 +10,13 @@ public class Login {
    LoginPage loginPage = new LoginPage();
 
         @Given("user is on the GMI bank login page")
-        public  void user_is_on_the_gmi_bank_login_page() {
+        public void user_is_on_the_gmi_bank_login_page() {
                 Driver.getDriver().get(ConfigReader.getProperty("gmibank_url"));
                 loginPage.sigin1.click();
                 loginPage.singin2.click();
+                loginPage.userName.sendKeys(ConfigReader.getProperty("valid_username"));
+                loginPage.password.sendKeys(ConfigReader.getProperty("valid_password"));
+                loginPage.signInButton.click();
 
 
         }
