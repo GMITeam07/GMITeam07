f68103fd6ee755c7c3e8b1328b1f71f48c83262a
taner77 <tcelikim@gmail.com>
2020-11-08 20:54:32 -0500

    steps login

--- src/test/java/steps/Login.java
+++ src/test/java/steps/Login.java
@@ -1,9 +1,23 @@
 package steps;
 
-public class LoginPage {
+import io.cucumber.java.en.Given;
+import pages.LoginPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
-    public static void login(){
+public class Login {
 
-    }
+   LoginPage loginPage = new LoginPage();
+
+        @Given("user is on the GMI bank login page")
+        public  void user_is_on_the_gmi_bank_login_page() {
+                Driver.getDriver().get(ConfigReader.getProperty("gmibank_url"));
+                loginPage.sigin1.click();
+                loginPage.singin2.click();
+
+
+        }
+
+
 
 }
