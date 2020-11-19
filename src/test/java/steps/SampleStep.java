ae79e3d99f062e2fbb95ce045d5ddd8e7de5a2c7
Nilgun <nilgund.yagci@gmail.com>
2020-11-18 23:22:39 -0500

    New update Wed

--- src/test/java/steps/SampleStep.java
+++ src/test/java/steps/SampleStep.java
@@ -1,5 +1,10 @@
 package steps;
 
+import io.cucumber.messages.internal.com.google.gson.JsonObject;
+import io.cucumber.messages.internal.com.google.protobuf.Api;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import pages.UserSettingsPage;
@@ -9,6 +14,7 @@ import utilities.Driver;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -64,5 +70,43 @@ public class SampleStep {
 //        System.out.println(ApiUtils.getCustomerById(2552));
 
     }
+    @Test
+    public void apiStates(){
+    int stateId = 19226;
+                Response response =
+                        given().
+                        auth().
+                        preemptive().
+                        basic("team07admin", "S123456s?").
+                        accept(ContentType.JSON).
+                        when().
+                        get("https://www.gmibank.com/api/tp-states/"+stateId);
+   //             response.prettyPrint();
+  //             System.out.println(response.getHeaders());
+        //       System.out.println("Status code: " + response.getStatusCode());
+  //             System.out.println(response.getBody());
 
-}
+  //      JsonPath jsonPath =response.jsonPath();
+  //      System.out.println(jsonPath.get("id").toString());
+
+//        State state = new State();
+//        state =response.as(State.class);
+//
+//        System.out.println(state.getId());
+//
+  //      List list = response.as(List.class);
+
+   //     System.out.println(list);
+
+//        List <State>list = response.jsonPath().getList("", State.class);
+//        System.out.println(list.get(0).getId());
+
+       State state =response.as(State.class);
+        System.out.println(state);
+
+
+    }
+    }
+
+
+
