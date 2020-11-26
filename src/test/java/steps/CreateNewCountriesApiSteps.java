003a79ddbb9bb048aa2a13e812b4afe1e532a156
Nilgun <nilgund.yagci@gmail.com>
2020-11-25 23:34:11 -0500

    2nd update

--- src/test/java/steps/CreateNewCountriesApiSteps.java
+++ src/test/java/steps/CreateNewCountriesApiSteps.java
@@ -8,15 +8,10 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
-import pages.CustomerCreationPage;
-import pages.RegistrationPage;
-import pojos.Customer;
-import specs.Specs;
-import utilities.ApiUtils;
+import pages.*;
 import utilities.ConfigReader;
-import utilities.DBUtilsNew;
+import utilities.Driver;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,92 +19,102 @@ import static io.restassured.RestAssured.given;
 
 public class CreateNewCountriesApiSteps {
 
+    CustomerCreationPage customerCreationPage = new CustomerCreationPage();
+    DataCreationPage dataCreationPage = new DataCreationPage();
+    LoginPage loginPage = new LoginPage();
+    LoginSteps loginSteps = new LoginSteps();
+    ManageCustomerPage manageCustomerPage = new ManageCustomerPage();
+
+
+
+    Demo29Page demo29Page= new Demo29Page();
     Response response;
     JsonPath json;
     List<Map<String, Object>> allCountryData;
+    List<Map<String, Object>> CountryName;
+
 
     @Given("user provides the api end point to set the response using by {string}")
     public void userProvidesTheApiEndPointToSetTheResponseUsingBy(String url) {
 
-
-        response = given().headers(
-                "Authorization",
-                "Bearer" + ConfigReader.getProperty("bearerToken"),
-                "Content-Type",
-                ContentType.JSON,
-                "Accept",
-                ContentType.JSON).auth()
+        response = given().
+                accept(ContentType.JSON).
+                auth()
                 .preemptive()
-                .basic("team07admin", "S123456s?")
+                .basic("team07user", "S123456s?")
                 .accept(ContentType.JSON)
                 .when()
                 .get(url)
                 .then()
                 .contentType(ContentType.JSON)
                 .extract()
-                .response();
-
-       // response.prettyPrint();
+                .response();;
+        response.prettyPrint();
+        json = response.jsonPath();
 
     }
+    @Given("user is on the Create Customer Page with valid credentials {string} {string}")
+    public void userIsOnTheCreateCustomerPageWithValidCredentials(String validemployee_username, String validemployee_password) {
+
+//        loginPage.loginDrpDwn.click();
+//        loginPage.signinOptionDrpDwn.click();
+//        loginPage.userName.sendKeys(validemployee_username);
+//        loginPage.password.sendKeys(validemployee_password);
+//        loginPage.signinSubmitButton.click();
+
+
+    }
+
+
 
     @And("user verifies the country {string} does not exist in database")
     public void userVerifiesTheCountryDoesNotExistInDatabase(String countryDNExist) {
 
-        //Assert.assertFalse(countryDNExist.contains("Egypt"));
+        Assert.assertFalse(countryDNExist.contains("Egypt"));
     }
+    @And("user clicks on My Operations button")
+    public void userClicksOnMyOperationsButton() {
 
-    @When("user creates a new country {string} {string} {string} {string} {string} {string} {string} {string} {string} {string} {string}")
-    public void userCreatesANewCountry(String ssn, String firstname, String lastname, String middleinitial, String email, String mobile, String zipcode, String address, String city, String country, String state) {
-
-
-        Map map=new HashMap();
-
-
-        map.put("ssn","234-048-8705");
-        map.put("firstName","Hale ");
-        map.put("lastName","Hug");
-        map.put("email","hh@gmail.com");
-        map.put("address","jonny st");
-        map.put("phonenumber","789-045-7321");
-        map.put("city","NorthYork");
-        map.put("country","Egypt");
-        map.put("state","ST");
-
-        Response response=given().
-                auth().
-                preemptive().
-                basic(
-                        ConfigReader.getProperty("validemployee_username"),
-                        ConfigReader.getProperty("validemployee_password")).
-                headers(map).
-
-                spec(Specs.specMainUrl()).
-                accept(ContentType.JSON).
-                when().
-                post("/api/customers");
-
-        response.prettyPrint();
-        }
-
-
-//        if(!firstname.equals("")) {
-//            CustomerCreationPage..clear();
-//            RegistrationPage.firstname.sendKeys(firstname);
-//        }
-//
-//        if(!lastname.equals("")) {
-//            createEditUserPage.lastname.clear();
-//            createEditUserPage.lastname.sendKeys(lastname);
-//        }
-//
-//
-
-
-
-    @Then("user verifies the country had been created in database")
-    public void userVerifiesTheCountryHadBeenCreatedInDatabase() {
+        manageCustomerPage.myOperations.click();
     }
 
 
+    @And("user clicks on Create a New Customer button")
+    public void userClicksOnCreateANewCustomerButton() {
+
+        manageCustomerPage.manageCostumer.click();
+
+
+    }
+
+    @When("user creates a new customer with new country {string} {string} {string} {string} {string} {string} {string} {string} {string} {string} {string}")
+    public void userCreatesANewCustomerWithNewCountry(String ssn, String firstname, String lastname, String middleinitial, String email, String mobile, String zipcode, String address, String city, String country, String state) {
+
+
+
+        Driver.getDriver().get("https://www.gmibank.com");
+
+
+//        customerCreationPage.ssn.sendKeys(ssn);
+//        customerCreationPage.firstname.sendKeys(firstname);
+//        customerCreationPage.lastname.sendKeys(lastname);
+//        customerCreationPage.email.sendKeys(email);
+//        customerCreationPage.mobile.sendKeys(mobile);
+//        dataCreationPage.zipCode.sendKeys(zipcode);
+//        customerCreationPage.address.sendKeys(address);
+//        dataCreationPage.city.sendKeys(city);
+//        dataCreationPage.customerCountry.sendKeys(country);
+//        dataCreationPage.customerState.sendKeys(state);
+
+
+    }
+    @Then("user clicks on save button")
+    public void userClicksOnSaveButton() {
+    }
+
+
+
+    @Then("user verifies the country had been created in database {string}")
+    public void userVerifiesTheCountryHadBeenCreatedInDatabase(String countryName) {
+    }
 }
--- target/test-classes/features/CreatingNewCountries.feature
+++ target/test-classes/features/CreatingNewCountries.feature
@@ -6,9 +6,13 @@ Feature: System should allow to create new countries using api end point if not 
 
      @NewCountries
     Scenario Outline: TC_2501 User can Just create each country 1 by 1
-          And user verifies the country "<countryName>" does not exist in database
-          When user creates a new country "<ssn>" "<firstname>" "<lastname>" "<middleinitial>" "<email>" "<mobile>" "<zipcode>" "<address>" "<city>" "<country>" "<state>"
-          Then user verifies the country had been created in database
+       Given user is on the Create Customer Page with valid credentials "validemployee_username" "validemployee_password"
+       And user verifies the country "<countryName>" does not exist in database
+       And user clicks on My Operations button
+       And user clicks on Create a New Customer button
+       When user creates a new customer with new country "<ssn>" "<firstname>" "<lastname>" "<middleinitial>" "<email>" "<mobile>" "<zipcode>" "<address>" "<city>" "<country>" "<state>"
+       Then user clicks on save button
+       Then user verifies the country had been created in database "<countryName>"
        #  Then user deletes the country "<countryName>"
 
        Examples:
